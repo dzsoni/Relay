141aab91ebdecf81b68c3ede0c1d2339b91da5df
dzsoni <fulekijan@gmail.com>
2024-03-13 18:36:38 +0100

    mediator client removed

--- Relay.cpp
+++ Relay.cpp
@@ -24,7 +24,6 @@ void Relay::On()
 {
     _state=_onstatelevel;
     _controlRelay();
-    Serial.println("RELAY IS ON");
 }
 
 void Relay::Off()
@@ -57,15 +56,7 @@ void Relay::init(unsigned char  pin, bool initstate, bool on_state_level)
     pinMode(_pin, OUTPUT);
     _controlRelay();
 }
-void Relay::initMqttClientCallbacks()
-{
-    //mclient.setOnConnect([this](bool sessionpresent){this->onConnect(sessionpresent);});
-    //mclient.setOnDisconnect([this](AsyncMqttClientDisconnectReason reason){this->onDisconnect(reason);});
-    mclient.setOnSubscribe([this](uint16_t packetId, uint8_t qos){this->onSubscribe(packetId,qos);});
-    mclient.setOnUnsubscribe([this](uint16_t packetid){this->onUnsubscribe(packetid);});
-    mclient.setOnMessage([this](char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total){this->onMessage(topic,payload,properties,len,index,total);});
-    mclient.setOnPublish([this](uint16_t packetId){this->onPublish(packetId);});
-}
+
 
 unsigned char Relay::getRelayPin()
 {
@@ -78,51 +69,4 @@ bool Relay::getOnStateLevel()
 bool Relay::getInitstate()
 {
     return _initstate;
-}
-
-//MQTT CALLBACKS
-void Relay::onConnect(bool sessionpresent)
-{
-Serial.println("RELAY ONCONNECT.");
-}
-void Relay::onDisconnect(AsyncMqttClientDisconnectReason reason)
-{
-Serial.println("RELAY ONDISCONNECT");
-}
-void Relay::onSubscribe( uint16_t packetId, uint8_t qos)
-{
-Serial.println("RELAY SUBSCRIBE");
-Serial.println("Subscribe acknowledged.");
-Serial.print("  packetId: ");
-Serial.println(packetId);
-Serial.print("  qos: ");
-Serial.println(qos);
-}
-void Relay::onUnsubscribe(uint16_t packetid)
-{
-Serial.println("RELAY ONUNSUBSCRIBE");
-}
-void Relay::onMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
-{
-Serial.println("RELAY ONMESSAGE");
- Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.print(topic);
-  Serial.print("  qos: ");
-  Serial.print(properties.qos);
-  Serial.print("  dup: ");
-  Serial.print(properties.dup);
-  Serial.print("  retain: ");
-  Serial.print(properties.retain);
-  Serial.print("  len: ");
-  Serial.print(len);
-  Serial.print("  index: ");
-  Serial.print(index);
-  Serial.print("  total: ");
-  Serial.print(total);
-  Serial.println("");
-}
-void Relay::onPublish(uint16_t packetId)
-{
-Serial.println("RELAY ONPUBLISH");
 }
--- Relay.h
+++ Relay.h
@@ -19,14 +19,11 @@ class Relay
 
     public:
 
-    MedClient          mclient;
-
     Relay(unsigned char pin, bool initstate, bool on_state_level);
     Relay(String pathToJson, String ID_of_relay ,bool initstate, bool on_state_level);
     ~Relay();
 
     void init(unsigned char pin, bool initstate, bool on_state_level);
-    void initMqttClientCallbacks();
     void On();
     void Off();
     void setRelayState(bool state);
@@ -34,13 +31,6 @@ class Relay
     bool getOnStateLevel();
     bool getInitstate();
     unsigned char getRelayPin();
-
-    void onConnect(bool sessionpresent);
-    void onDisconnect(AsyncMqttClientDisconnectReason reason);
-    void onSubscribe(uint16_t packetId, uint8_t qos);
-    void onUnsubscribe( uint16_t packetid);
-    void onMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
-    void onPublish(uint16_t packetId);
 };
 
 #endif
